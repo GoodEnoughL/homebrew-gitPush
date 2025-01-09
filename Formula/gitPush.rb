6ae65b3c71f0569128bc295b03ee861add78e5d4
Guo Liang Hou <guoliang.hou@npt.sg>
2025-01-09 18:55:56 +0800

    change file name

--- Formula/gitPush.rb
+++ Formula/gitPush.rb
@@ -1,12 +1,12 @@
 class GitPush < Formula
     desc "A helpful git push tool" 
     homepage "https://github.com/GoodEnoughL/gitPush"
-    url "https://github.com/GoodEnoughL/gitPush/releases/download/v1.0.0/git-tools-darwin-amd64@v1.0.0"
+    url "https://github.com/GoodEnoughL/gitPush/releases/download/v1.0.0/gitPush-darwin-amd64"
     sha256 "0392e8c0b7ecef408742988978f6a5a96e5c15812f2eef5031aa87da61b3f877"
     license "MIT"
   
     def install
-        bin.install "git-tools-darwin-amd64@v1.0.0" => "gitPush"
+        bin.install "gitPush-darwin-amd64" => "gitPush"
     end
     
     test do
