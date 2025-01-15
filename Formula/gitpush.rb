7dd29c8577aa27bdb8c72760038b486d27d64a32
Guo Liang Hou <guoliang.hou@npt.sg>
2025-01-15 10:59:15 +0800

    fix command

--- Formula/gitpush.rb
+++ Formula/gitpush.rb
@@ -2,7 +2,7 @@ class Gitpush < Formula
     desc "A helpful git push tool" 
     homepage "https://github.com/GoodEnoughL/gitPush"
     url "https://github.com/GoodEnoughL/gitPush/releases/download/v1.0.0/gitPush-darwin-amd64"
-    sha256 "409953e876607f08c6ceb57fbd7cc343f3767aaa91097aef61f2365c04695cab"
+    sha256 "4281f90ea4c5a513feca31fb5c7e346a2d3f20642c6ef3228618f19a2e2b5492"
     license "MIT"
   
     def install
